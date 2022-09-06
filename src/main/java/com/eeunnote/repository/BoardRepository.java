d6927873440bdc93c0183010f6bc88cf298b7ba5
eeun <eeun95@gmail.com>
2022-09-06 16:19:20 +0900

    commit

--- src/main/java/com/eeunnote/repository/BoardRepository.java
+++ src/main/java/com/eeunnote/repository/BoardRepository.java
@@ -1,4 +1,15 @@
 package com.eeunnote.repository;
 
-public interface BoardRepository {
+import com.eeunnote.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    Board save(Board board);
+    Optional<Board> findByNo(Long no);
+    Optional<Board> findByTitle(String title);
+    List<Board> findAll();
 }
